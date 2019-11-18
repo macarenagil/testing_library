a2a2d8f4bb1c28fdee45ec91ac2bc52edf9db8f9
Macarena Arroyo Gil <macarena@blinkhealth.com>
2019-11-18 17:49:44 -0500

    first commit

---
+++ app/lib.js
@@ -0,0 +1,13 @@
+function it (message, fn) {
+    console.log(message)
+    fn()
+}
+
+function should (condition, ) {
+if (condition) {
+console.log('✔')
+}
+else {
+    console.log('✗')
+}
+}
---
+++ app/test.js
@@ -0,0 +1,7 @@
+it('Should be true', () => {
+should( 1 === 1)
+})
+
+it('Should be false', () => {
+    should( 1 !== 1)
+    })
